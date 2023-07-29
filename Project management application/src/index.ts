13cbf17e3f85a2c28ef33dabe40e200fbbd0995a
Alexandr Prokopovich <prokop7@mail.ru>
2023-07-29 16:59:08 +0300

    Add mongodb

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,19 +3,18 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      'mongodb+srv://Admin123:Admin123@cluster0.hjx0lx6.mongodb.net/'
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
